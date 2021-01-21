25eac2b4fa0386fb984143e7bf1aae1e58eb8e56
詹楚江 <chujiang.zhan@dreampix.cn>
2021-01-21 16:38:59 +0800

    second

--- models/connect.js
+++ models/connect.js
@@ -1,13 +1,19 @@
 // 引入mongoose第三方模块
 const mongoose = require('mongoose');
 // 连接数据库
-mongoose.connect('mongodb://localhost/manage', {useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/manage', {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true })
 	.then(() => console.log('数据库连接成功'))
   .catch(() => console.log('数据库连接失败'))
 
 UserSchema = new mongoose.Schema({
-  user:{type:String},
-  password:{type:String}
+  user:{ type:String, unique:true },
+  password:{
+    type:String,
+    set(val){
+      // 加密处理
+      return require('bcrypt').hashSync(val,10)
+    }
+  }
 })
 const User = mongoose.model('User',UserSchema)
 
--- route/user.js
+++ route/user.js
@@ -2,20 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Reslut = require('../models/Result')
 const { User } = require('../models/connect')
-
-router.get('/info',(req,res,next) => {
-  res.json('user info....')
-})
-
-
+const jwt = require('jsonwebtoken')
+//密钥
+const SELRET = 'sakshkKHKHsdfsdfsd'
+//注册接口
 router.post('/register',async(req,res,next) => {
   console.log(req.body)
   // const { user, password } = req.body
   
   let user = await User.findOne({user:req.body.user})
   if(user) {
-    new Reslut('注册失败','用户已注册，注册失败!').fail(res)
-    
+    new Reslut('注册失败','用户名已存在，请重新注册!').fail(res)
   }else{
     await User.create({
       user:req.body.user,
@@ -23,33 +20,43 @@ router.post('/register',async(req,res,next) => {
     })
     new Reslut('注册成功','注册成功,请登录!').success(res)
   }
-  
-  // res.json({
-  //   code: 0,
-  //   msg:'登陆成功'
-  // })
 })
 
-router.post('/login',(req,res,next) => {
+//登录接口
+router.post('/login',async(req,res,next) => {
   console.log(req.body)
   const {user, password} = req.body
-  let data = {
-    token:'shdakasydbjadfg'
+  // 使用传过来的用户名查询用户是否存在  并存下来
+  const username = await User.findOne({
+    user:user
+  })
+  if (!username) {
+        // res.status(422)
+    new Reslut({},'用户不存在').fail(res)
   }
-  if (user ==='admin'&& password ==='admin') {
+  // 确认请求密码和数据库的密码是否正确
+  const isPasswordValid = require('bcrypt').compareSync(password,username.password )
+  if (isPasswordValid){
+    const token = jwt.sign({
+      id: String(username._id),
+    },SELRET)
+    let data = {
+      token
+    }
     new Reslut(data,'登录成功').success(res)
   }else{
-    new Reslut(data,'登录失败').fail(res)
+    new Reslut({},'密码错误').fail(res)
   }
 
-  
-  // res.json({
-  //   code: 0,
-  //   msg:'登陆成功'
-  // })
 })
+
+//退出接口
 router.post('/logout',(req,res,next) => {
   new Reslut('退出成功').success(res)
   console.log(new Reslut('退出成功').success(res))
+  // res.json({
+  //   code: 0,
+  //   msg:'登陆成功'
+  // })
 })
 module.exports = router
